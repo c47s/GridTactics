74bdd9a221978f547e986354e174feb353934abb
c47s <36133719+c47s@users.noreply.github.com>
2022-02-23 19:59:21 -0500

    Create API module

---
+++ src/API.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators #-}
+
+module API where
+
+import Mechanics
+import Servant.API
+
--- src/GridTactics.hs
+++ src/GridTactics.hs
@@ -1,14 +1,5 @@
-{- |
-Copyright: (c) 2022 c47s
-SPDX-License-Identifier: MIT
-Maintainer: c47s <36133719+c47s@users.noreply.github.com>
+module GridTactics (module GridTactics) where
 
-See README for more info
--}
-
-module GridTactics
-       ( module GridTactics
-       ) where
-
+import API as GridTactics
 import Mechanics as GridTactics
 import SeqWorld as GridTactics
