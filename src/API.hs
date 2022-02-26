2382ecf1021ca90a8f87be1e5e1a733ad9a876ec
c47s <36133719+c47s@users.noreply.github.com>
2022-02-25 19:56:11 -0500

    Start implementing server

--- src/API.hs
+++ src/API.hs
@@ -1,34 +1,52 @@
--- {-# LANGUAGE FlexibleInstances #-}
 -- {-# LANGUAGE OverloadedStrings #-}
 
 module API
    ( API
+   , runServer
    ) where
 
 import Mechanics
+import Network.Wai.Handler.Warp
 import Relude
-import Servant.API
--- import Web.HttpApiData
-
--- Wrap types in Res to have them automatically enc/decoded to HttpApiData
--- using their Show, Bounded, and Enum instances.
--- newtype Res a = Res { unRes :: a }
-
--- instance (Show a) => ToHttpApiData (Res a) where
---    toUrlPiece = showTextData . unRes
--- instance (Show a, Bounded a, Enum a) => FromHttpApiData (Res a) where
---    parseUrlPiece = fmap Res . parseBoundedTextData
+import Servant
+import WebInstances
 
 type Modify ts a = Get ts a
-              :<|> ReqBody ts a :> Put ts ()
+              :<|> ReqBody ts a :> PostNoContent
 
 type ActorAPI = Get '[JSON] Actor
-           :<|> "view" :> Get '[JSON] Grid
+           :<|> "view" :> Get '[JSON] Grid -- View the World
            :<|> "act"  :> ReqBody '[JSON] Action :> Post '[JSON] Grid
            :<|> "done" :> Modify '[JSON] Bool
 
 type ActorsAPI = "names" :> Get '[JSON] [Text]
-            :<|> "new"   :> ReqBody '[JSON] Text :> Post '[JSON] UID
+            :<|> "new"   :> ReqBody '[JSON] Text :> Post '[JSON] UID -- Provide a name
+            :<|> "done"  :> Get '[JSON] Int -- Number marked as done
 
 type API = "actor"  :> Capture "aID" UID :> ActorAPI
       :<|> "actors" :> ActorsAPI
+
+runServer :: (World w) => w -> IO ()
+runServer w = do
+   wRef <- newIORef w
+   
+   let hView = undefined
+   let hAct = undefined
+   let hDone = undefined
+
+   let hNames = undefined
+   let hNew = undefined
+   let hDoneNum = undefined
+
+   let hActor aID = hView aID
+               :<|> hAct aID
+               :<|> hDone aID
+
+   let hActors = hNames
+            :<|> hNew
+            :<|> hDoneNum
+
+   let hAPI = hActor
+         :<|> hActors
+
+   run 8081 (serve (Proxy :: Proxy API) hAPI)
