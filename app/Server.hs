643dd5165627ef87466d139428d07d7ed3f5a156
c47s <36133719+c47s@users.noreply.github.com>
2022-03-01 22:10:55 -0500

    Adorn server startup UI

--- app/Server.hs
+++ app/Server.hs
@@ -13,14 +13,20 @@ gr0 = check "Must be greater than zero." (> 0)
 
 main :: IO ()
 main = runInputT defaultSettings do
+    outputStrLn "Hello!"
+    outputStrLn $ "Welcome to the " ++ productName ++ " server."
+    
+    outputStrLn ""
     port <- untilValidAnd nonNeg do
         outputStrLn "Enter port:"
         getInputLineWithInitial "> " ("42069","")
 
+    outputStrLn ""
     wSize <- untilValidAnd gr0 do
         outputStrLn "Enter world radius:"
         getInputLineWithInitial "> " ("10","")
 
+    outputStrLn ""
     fillPortion <- untilValidAnd
         ( gr0
         &>- check "Cannot be greater than 100 percent." (<= 100)
@@ -28,33 +34,40 @@ main = runInputT defaultSettings do
             outputStrLn "Enter percentage of the map to fill with scatters:"
             getInputLineWithInitial "> " ("50","")
 
+    outputStrLn ""
     scatterHealth <- untilValidAnd nonNeg do
         outputStrLn "Enter scatter health:"
         getInputLineWithInitial "> " ("2","")
 
+    outputStrLn ""
     scatterActions <- untilValidAnd nonNeg do
         outputStrLn "Enter scatter actions:"
         outputStrLn "(Action points that can be looted from a scatter)"
         getInputLineWithInitial "> " ("1","")
 
+    outputStrLn ""
     startHealth <- untilValidAnd gr0 do
         outputStrLn "Enter starting health:"
         getInputLineWithInitial "> " ("3","")
 
+    outputStrLn ""
     startMaxHealth <- untilValidAnd
         (check "Cannot be less than starting health." (>= startHealth))
         do
             outputStrLn "Enter starting maximum health:"
             getInputLineWithInitial "> " (show startHealth,"")
 
+    outputStrLn ""
     startActions <- untilValidAnd gr0 do
         outputStrLn "Enter starting actions:"
         getInputLineWithInitial "> " ("1","")
 
+    outputStrLn ""
     startRange <- untilValidAnd gr0 do
         outputStrLn "Enter starting range:"
         getInputLineWithInitial "> " ("2","")
 
+    outputStrLn ""
     startVision <- untilValidAnd gr0 do
         outputStrLn "Enter starting vision distance:"
         getInputLineWithInitial "> " ("3","")
@@ -92,4 +105,6 @@ main = runInputT defaultSettings do
                 }
             }
 
+    outputStrLn "\nStarting Server..."
     liftIO $ runServer port (w :: SeqWorld) conf
+    outputStrLn "Goodbye!"
