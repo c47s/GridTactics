09badf6bd4f71a6ce45de5957ccc05d1d6f917e1
c47s <36133719+c47s@users.noreply.github.com>
2022-03-01 22:10:42 -0500

    Add productName for easy renaming

--- src/GridTactics.hs
+++ src/GridTactics.hs
@@ -1,6 +1,14 @@
-module GridTactics (module GridTactics) where
+module GridTactics
+    ( module GridTactics
+    , productName
+    ) where
 
 import API as GridTactics
 import Mechanics as GridTactics
 import SeqWorld as GridTactics
 import Util as GridTactics
+
+import Relude
+
+productName :: IsString a => a
+productName = "GridTactics"
