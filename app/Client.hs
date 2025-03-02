b1ef6cacbed9d78e2b2719e7fbad3ad508b3e424
c47s <36133719+c47s@users.noreply.github.com>
2025-03-02 16:27:39 -0500

    No more debug cheats!

--- app/Client.hs
+++ app/Client.hs
@@ -8,7 +8,7 @@ import           Brick.Main
 import           Control.Concurrent (forkIO, threadDelay)
 import qualified Data.Bimap as Bap
 import           Data.List.Extra
-import qualified Data.Sequence as Seq
+-- import qualified Data.Sequence as Seq
 import qualified Data.Text as T
 import           Data.Time.Clock
 import           Data.Time.Format
@@ -160,13 +160,16 @@ doUIAction (SwitchTo aID) s = continue' $ s {actorIDs = rotateTo aID $ actorIDs 
 doUIAction ToggleDone s = toggleDone (currActorID s) s
 doUIAction SubmAction s = inApp s (act (currActorID s) (currAction s)) >> continue' s
 doUIAction DelAction s = inApp s (delAct $ currActorID s) >> continue' s
-doUIAction ClaimAllPawns s = do
-    allActors <- inApp s getActorIDs
-    continue' $ s {actorIDs = fromList allActors}
-doUIAction RemovePawn s = do
-    _ <- inApp s . quitActor $ currActorID s
-    continue' $ s {actorIDs = Seq.filter (/= currActorID s) $ actorIDs s}
-doUIAction DisownPawn s = continue' $ s {actorIDs = Seq.filter (/= currActorID s) $ actorIDs s}
+doUIAction ClaimAllPawns _s = continueWithoutRedraw
+    -- do
+    -- allActors <- inApp s getActorIDs
+    -- continue' $ s {actorIDs = fromList allActors}
+doUIAction RemovePawn _s = continueWithoutRedraw
+    -- do
+    -- _ <- inApp s . quitActor $ currActorID s
+    -- continue' $ s {actorIDs = Seq.filter (/= currActorID s) $ actorIDs s}
+doUIAction DisownPawn _s = continueWithoutRedraw
+    -- continue' $ s {actorIDs = Seq.filter (/= currActorID s) $ actorIDs s}
 doUIAction Quit s = quit s
 doUIAction (Also uiA) s = doUIAction uiA s
 
